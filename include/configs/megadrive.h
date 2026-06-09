58351542bd043e47a2b41f444ab84fe493d7d086
Daniel Palmer <daniel@thingy.jp>
2026-06-09 21:23:23 +0900

    megadrive: default env: start booting linux

--- include/configs/megadrive.h
+++ include/configs/megadrive.h
@@ -8,8 +8,9 @@
 #define CFG_SYS_INIT_RAM_SIZE 0x380000
 
 #define CFG_EXTRA_ENV_SETTINGS \
-		"bootcmd=everdrive disk init; everdrive disk load 0x180000 vmlinux.lz4;" \
-		"unlz4 0x180000 0x1000 0x180000\0" \
+		"bootcmd=everdrive disk init; everdrive disk load 0x1000 vmlinux.lz4;" \
+		"unlz4 0x1000 0x180000 0x180000;" \
+		"bootelf 0x180000\0" \
 		"stdout=serial,vidconsole\0" \
 		"autostart=yes\0" \
 		"bootargs=earlycon=xxx\0"
