9da66c43e96bcc7f0839171d3226c2637ae88493
Daniel Palmer <daniel@thingy.jp>
2026-06-18 21:16:14 +0900

    megadrive default env

--- include/configs/megadrive.h
+++ include/configs/megadrive.h
@@ -14,4 +14,4 @@
 		"bootelf -p 0x180000\0" \
 		"stdout=serial,vidconsole\0" \
 		"autostart=yes\0" \
-		"bootargs=earlyprintk console=ttyVDP0 console=ttyED0 root=/dev/edblk -- smolinit.getty=/dev/ttyED0 smolinit.hostname=md\0"
+		"bootargs=earlyprintk console=ttyVDP0 console=ttyED0 root=/dev/edblk -- smolinit.getty=/dev/ttyED0 smolinit.getty=/dev/ttyVDP0 smolinit.hostname=md\0"
