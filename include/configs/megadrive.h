1d0043059dadbb2b63a2c79050ae100f9c30a42e
Daniel Palmer <daniel@thingy.jp>
2026-06-24 20:13:54 +0900

    md def environment

--- include/configs/megadrive.h
+++ include/configs/megadrive.h
@@ -3,7 +3,7 @@
 /*
  * We want to map the save SRAM into the last 512KB
  * slot so the most RAM we can map into the cart address
- * space is 3.5MB
+ * space is 3.5MB - note, I disabled this for now as we need the RAM..
  */
 #define CFG_SYS_INIT_RAM_SIZE 0x380000
 
@@ -14,4 +14,4 @@
 		"bootelf -p 0x180000\0" \
 		"stdout=serial,vidconsole\0" \
 		"autostart=yes\0" \
-		"bootargs=earlyprintk console=ttyVDP0 console=ttyED0 root=/dev/edblk -- smolinit.getty=/dev/ttyED0 smolinit.getty=/dev/ttyVDP0 smolinit.hostname=md\0"
+		"bootargs=console=ttyVDP0 console=ttyED0 root=/dev/edblk -- smolinit.getty=/dev/ttyED0 smolinit.getty=/dev/ttyVDP0 smolinit.hostname=md\0"
